e669c2d2098d75412e2d9bacda6ee9c26f0698fe
gojoy <729324352@qq.com>
2018-01-13 16:46:40 +0800

    update windows

---
+++ exam/didi/mianshi/mysort_test.go
@@ -0,0 +1,26 @@
+package mianshi
+
+import (
+	"testing"
+	"math/rand"
+	"sort"
+	"fmt"
+)
+
+func Test_qsort(t *testing.T)  {
+	list:=make([]int,0)
+	for i:=0;i<100;i++ {
+		list=append(list,rand.Int())
+		//sort.Ints(list)
+		quickSort(list,0,len(list)-1)
+		if sort.IntsAreSorted(list) {
+			t.Log("qosrt ok\n")
+		} else {
+			t.Error("qsort error\n")
+			for _,v:=range list {
+				fmt.Printf("%v  ",v)
+			}
+			fmt.Println()
+		}
+	}
+}
